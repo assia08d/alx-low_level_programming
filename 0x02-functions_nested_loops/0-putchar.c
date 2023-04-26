58e49129f925a86e6123369cd0ef59d319cb119d
Asmae Daghbach <assiade08@gmail.com>
2023-04-26 15:15:20 -0700

    putchar?

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print_putchar
+ * is written by assia08d
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p')
+_puchar('u')
+_putchar('t')
+_putchar('c')
+_putchar('h')
+_putchar('a')
+_putchar('r')
+_putchar('\n')
+return (0);
+}
